9572f97081bb4516d4d8d398559027799632e596
RealMG <realmg51@gmail.com>
2026-04-03 21:45:04 +0700

    Update main.logic.cs

--- src/main/main.logic.cs
+++ src/main/main.logic.cs
@@ -1,1 +1,31 @@
-//
+using System;
+namespace NtAI
+{
+  class Logic
+  {
+    static void Call()
+    {
+      bool run = true;
+      while(true)
+      {
+        Console.Write("You: ");
+        string input = Console.ReadLine()?.Trim().ToLower() ?? "";
+        Console.Write("AI: ");
+        if(input == "exit" || input == "quit")
+        {
+          run = false;
+        }
+        else if (string.IsNullOrWhiteSpace(input))
+        {
+          Console.WriteLine("Sorry,I can't hear that.");
+        }
+
+        else
+        {
+          Console.WriteLine($"I don't know about '{input}',please talk about something else");
+        }
+      }
+    }
+  }
+}
+}·
